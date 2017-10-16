55b703f3657360016881d00b55a032d77b8ee056
Batalex <batisse.alexandre@gmail.com>
2017-10-16 15:59:57 +0200

    more refactor TODO fix price & conv

--- main.py
+++ main.py
@@ -42,275 +42,247 @@ def case(*args):
     return any((arg == switch.value for arg in args))
 
 
-def calc(str):
+def calc(str_usr):
     """ Calc command """
-    str = str.replace('os.', '')
-    str = str.split()
-    str = str.remove('calc')
+    str_usr = str_usr.replace('os.', '')
+    str_usr = str_usr.split()
+    str_usr = str_usr.remove('calc')
     return ''
 
 
-def traitement(str):
+def traitement(str_usr):
     """ Using our custom switch on user entry """
     d = 0
-    str = str.lower()
-    if len(str.split()) > 1:
-        while switch((str.split()[0]).lower()):
+    str_usr = str_usr.lower()
+    if len(str_usr.split()) > 1:
+        while switch((str_usr.split()[0]).lower()):
             if case('prix'):
-                d = price(str.split())
+                d = price(str_usr.split())
                 break
             if case('price'):
-                d = price(str.split())
+                d = price(str_usr.split())
                 break
             if case('conv'):
-                d = conv(str.split())
+                d = conv(str_usr.split())
                 break
             if case('volume'):
                 break
             if case('chart'):
-                d = chart.chart(str.split()[1])
+                d = chart.chart(str_usr.split()[1])
                 break
             if case('book'):
-                d = chart.book(str.split()[1])
+                d = chart.book(str_usr.split()[1])
                 break
             break
     return d
 
-def price(str):
+def price(str_usr):
     """ Break down price command """
-    if len(str) >= 2:
-        while switch(str[1]):
+    if len(str_usr) >= 2:
+        while switch(str_usr[1]):
             if case('polo'):
-                str.remove('polo')
-                t = poloniex(str)
+                str_usr.remove('polo')
+                t = poloniex(str_usr)
                 break
             if case('trex'):
-                str.remove('trex')
-                t = bittrex(str)
+                str_usr.remove('trex')
+                t = bittrex(str_usr)
                 break
             if case('poloniex'):
-                str.remove('poloniex')
-                t = poloniex(str)
+                str_usr.remove('poloniex')
+                t = poloniex(str_usr)
                 break
             if case('bittrex'):
-                str.remove('bittrex')
-                t = bittrex(str)
+                str_usr.remove('bittrex')
+                t = bittrex(str_usr)
                 break
             if case('p'):
-                str.remove('p')
-                t = poloniex(str)
+                str_usr.remove('p')
+                t = poloniex(str_usr)
                 break
             if case('b'):
-                str.remove('b')
-                t = bittrex(str)
+                str_usr.remove('b')
+                t = bittrex(str_usr)
                 break
-            t = lost(str)
+            t = lost(str_usr)
             break
     return t
 
-def conv(boo2):
+def conv(str_usr):
 
-    boo2.remove('conv')
-    valBtc = btcrecup(0)
-    valBtcE = btcrecup(1)
+    str_usr.remove("conv")
+    val_btc, val_btc_e = btcrecup()
+
     try:  #si le nombre + la devise
-        float(boo2[0])
-        final=polorecup(boo2[1], 0)
+        float(str_usr[0])
+        if str_usr[1].upper() == 'BTC':
+            amount_dollars = val_btc * float(str_usr[0])
+            amount_euros = val_btc_e * float(str_usr[0])
+            verb = 'vaut' if str_usr[0] == '1' else 'valent'
+            ret = '```{} {} {} {:.2f}$ ou {:.2f}€```'.format(
+                str_usr[0],
+                str_usr[1].upper(),
+                verb,
+                amount_dollars,
+                amount_euros
+            )
+            return ret
+        final = polorecup(str_usr[1], 0)
+        if not final:
+            final = bittrecup(str_usr[1], 0)
+        if not final:
+            final = finexrecup(str_usr[1], 0)
 
-        if(boo2[1].upper()=="BTC"):
-            final=float(valBtc)*float(boo2[0])
-            final2=float(valBtcE)*float(boo2[0])
-            return ("```"+str(boo2[0])+" "+str(boo2[1]).upper()+" valent "+ str("%.2f" %final)+"$ ou "+str("%.2f" %final2)+"€```")
-
-        if(final==0):
-            final=bittrecup(boo2[1],0)
-        if(final!=0):
-            final2=(float(valBtcE)*(float(final)/float(valBtc)))*float(boo2[0])
-            final=float(final)*float(boo2[0])
-            final = "```"+str(boo2[0])+" "+str(boo2[1]).upper()+" valent "+ str("%.2f" %final)+"$ ou "+str("%.2f" %final2)+"€  ("+str("%.4f" %(final/valBtc))+"฿)```"
-
-    except : #si la devise + le nombre
+        if final:
+            amount_dollars = float(final) * float(str_usr[0])
+            amount_euros = float(final) * float(str_usr[0]) * val_btc_e / val_btc
+            verb = 'vaut' if str_usr[0] == '1' else 'valent'
+            ret = '```{} {} {} {:.2f}$ ou {:.2f}€ ({:.4f}฿)```'.format(
+                str_usr[0],
+                str_usr[1].upper(),
+                verb,
+                amount_dollars,
+                amount_euros,
+                amount_dollars / val_btc
+            )
+        else:
+            ret = 'Je n\'ai pas trouvé la monnaie {}.'.format(str_usr[1].upper())
+    except: #si la devise + le nombre
         try:
-            float(boo2[1])
+            float(str_usr[1])
         except:
-            boo2[1]=0
+            str_usr[1] = 0
 
-        if(boo2[0].upper()=="BTC"):
-            final=float(valBtc)*float(boo2[1])
-            final2=float(valBtcE)*float(boo2[1])
-            print(1)
-            return ("```"+str(boo2[1])+" "+str(boo2[0]).upper()+" valent "+ str("%.2f" %final)+"$ ou "+str("%.2f" %final2)+"€```")
+        if str_usr[0].upper() == 'BTC':
+            amount_dollars = val_btc * float(str_usr[1])
+            amount_euros = val_btc_e * float(str_usr[1])
+            verb = 'vaut' if str_usr[1] == 1 else 'valent'
+            ret = '```{} {} {} {:.2f}$ ou {:.2f}€```'.format(
+                str_usr[1],
+                str_usr[0].upper(),
+                verb,
+                amount_dollars,
+                amount_euros
+            )
+            return ret
 
-        final=polorecup(boo2[0],0)
-        if(final==0):
-            final=bittrecup(boo2[0],0)
+        final = polorecup(str_usr[0], 0)
+        if not final:
+            final = bittrecup(str_usr[0], 0)
+        if not final:
+            final = finexrecup(str_usr[0], 0)
 
-        if(final!=0):
-            final2=(float(valBtcE)*(float(final)/float(valBtc)))*float(boo2[1])
-            final=float(final)*float(boo2[1])
-            final ="```"+str(boo2[1])+" "+str(boo2[0]).upper()+" valent "+  str("%.2f" %final)+"$ ou "+str("%.2f" %final2)+"€  ("+str("%.4f" %(final/valBtc))+"฿)```"
-
-    return final
-
-
-
-def bittrex(boo3):
-
-    print("Trex Selector")
-
-    i = 0
-    final = ["","","","","",""]
-    boo3.remove("price")
-
-    nbreCase = len(boo3)
-
-    offset = 0
-
-    while nbreCase > i :
-
-        final[offset]=bittrecup(boo3[i],1)
-
-        if(final[offset]!=0):
-            offset+=1
-
-        i = 1 + i
-
-    return final
-
-def poloniex(boo3):
-
-    print("Polo Selector")
-
-    i = 0
-    final = ["","","","","","","","","","","","","","","",""]
-
-    boo3.remove("price")
-
-
-    nbreCase = len(boo3)
-
-    offset=0
-
-    while nbreCase > i :
-
-        final[offset]=polorecup(boo3[i],1)
-
-        if(final[offset]!=0):
-            offset+=1
-
-        i = 1 + i
-    return final
-
-def finexrecup(boo4,all):
-
-    url="https://api.bitfinex.com/v1/pubticker/"
-    valBtc = btcrecup(0)
-    market=boo4+"btc"
-    print(market)
-    content=requests.get(url+market)
-    data=content.json()
-
-    if("bid" in data):
-        if(all):
-            return("```"+boo4.upper()+"   "+str(data["last_price"])+"฿    $"+str("%.5f" %(float(data["last_price"])*float(valBtc)))+"  (Bitfinex)"+"```")
+        if final:
+            amount_dollars = float(final) * float(str_usr[1])
+            amount_euros = float(final) * float(str_usr[1]) * val_btc_e / val_btc
+            verb = 'vaut' if str_usr[1] == 1 else 'valent'
+            ret = '```{} {} {} {:.2f}$ ou {:.2f}€ ({:.4f}฿)```'.format(
+                str_usr[1],
+                str_usr[0].upper(),
+                verb, amount_dollars,
+                amount_euros,
+                amount_dollars / val_btc
+            )
         else:
-            return(data["last_price"]*valBtc)
-    else:
-        return 0
+            ret = 'Je n\'ai pas trouvé la monnaie {}.'.format(str_usr[0].upper())
+    return ret
 
-
-def lost(boo3):
-
-    i = 0
-    final = ["","","","","","","","","","","","","","","",""]
-    boo3.remove("price")
-
-    nbreCase = len(boo3)
-
-    while nbreCase > i :
-
-        final[i]=polorecup(boo3[i],1)
-
-        if(final[i]==0):
-            final[i]=bittrecup(boo3[i],1)
-        if(final[i]==0):
-            final[i]=finexrecup(boo3[i],1)
-
-        i = 1 + i
+def bittrex(str_usr):
+    str_usr.remove('price')
+    final = [bittrecup(cur, 1) for cur in str_usr]
+    final = [fin for fin in final if fin]
     return final
 
-def polorecup(boo4,all):
+def poloniex(str_usr):
+    str_usr.remove('price')
+    final = [polorecup(cur, 1) for cur in str_usr]
+    final = [fin for fin in final if fin]
+    return final
 
-    market="BTC_"+boo4.upper()
-
-    valBtc = btcrecup(0)
-    valBtcE = btcrecup(1)
-
-    if(market=="BTC_BTC"):
-        value = "```1 BTC vaut "+str(valBtc)+"$"+" ou "+str(valBtcE)+"€```"
-        return(value)
-
-    url="https://poloniex.com/public?command=returnTicker"
-    print("Poloniex Récup")
-
-    content=requests.get(url)
-    data=content.json()
-
-
-
-    print(market)
-
-    if(market in data):
-        if(all):
-            return("```"+boo4.upper()+"   "+data[market]["last"]+"฿ ("+str("%.2f" %(float(data[market]["percentChange"])*100))+"%) $"+(str("%.5f" %(float(data[market]["last"])*(float(valBtc)))))+" (Poloniex)"+"```")
-        else:
-            return(float(data[market]["last"])*valBtc)
-    else:
-        return 0
-
-
-def bittrecup(boo4,all):
-
-    print("Bittrex Recup")
-
-    url="https://bittrex.com/api/v1.1/public/getmarketsummary?market="
-    valBtc = btcrecup(0)
-    market="btc-"+boo4
-    print(market)
-    content=requests.get(url+market)
-    data=content.json()
-
-    print(data["success"])
-
-    if(data["success"]):
-
-        if(all):
-            print("all")
-            percent1=((data["result"][0]["Last"])-(data["result"][0]["PrevDay"]))
-            percent2=(percent1/float(data["result"][0]["PrevDay"]))*100
-
-
-
-            return("```"+boo4.upper()+"   "+str(data["result"][0]["Last"])+"฿ ("+str("%.2f" %percent2)+"%) $"+str("%.5f" %(data["result"][0]["Last"]*float(valBtc)))+"  (Bittrex)"+"```")
-        else:
-            return(data["result"][0]["Last"]*valBtc)
-    else:
-        return 0
-
-def btcrecup(euro):
-    """ Get BTC price"""
-    if(euro):
-        url = 'https://www.bitstamp.net/api/v2/ticker/btceur/'
-    else:
-        url = 'https://www.bitstamp.net/api/v2/ticker/btcusd/'
+def finexrecup(str_usr, verbose):
+    market = '{}btc'.format(str_usr)
+    url = 'https://api.bitfinex.com/v1/pubticker/{}'.format(market)
+    val_btc, _ = btcrecup()
     content = requests.get(url)
     data = content.json()
-    if ("last" in data):
-        return (float(data["last"]))
+    if ("bid" in data):
+        if verbose:
+            ret = '```{} {}฿ ${:.5f} (Bitfinex)```'.format(
+                str_usr.upper(),
+                data['last_price'],
+                val_btc * data['last_price']
+            )
+        else:
+            ret = data['last_price'] * val_btc
     else:
-        return 0
+        ret = 0
+    return ret
 
 
+def lost(str_usr):
+    str_usr.remove('price')
+    ret = []
+    for cur in str_usr:
+        fin = polorecup(cur, 1)
+        if not fin:
+            bittrecup(cur, 1)
+        if not fin:
+            finexrecup(cur, 1)
+        if fin:
+            ret.append(fin)
+    return ret
+
+def polorecup(str_usr, verbose):
+    market = 'BTC_{}'.format(str_usr.upper())
+    val_btc, val_btc_e = btcrecup()
+    if market == 'BTC_BTC':
+        ret = '```1 BTC vaut {}$ ou {}€```'.format(val_btc, val_btc_e)
+        return ret
+    url = 'https://poloniex.com/public?command=returnTicker'
+    content = requests.get(url)
+    data = content.json()
+    if data[market]:
+        if verbose:
+            ret = '```{} {}฿ ({:.2f}%) ${:.5f} (Poloniex)```'.format(
+                str_usr.upper(),
+                data[market]['last'],
+                data[market]['percentChange'] * 100,
+                data[market]['last'] * val_btc
+            )
+        else:
+            ret = val_btc * data[market]['last']
+    else:
+        ret = 0
+    return ret
+
+
+def bittrecup(str_usr, verbose):
+    market = 'btc-{}'.format(str_usr)
+    url = 'https://bittrex.com/api/v1.1/public/getmarketsummary?market={}'.format(market)
+    val_btc, _ = btcrecup()
+    content = requests.get(url)
+    data = content.json()
+    if data['success']:
+        if verbose:
+            perc = 100 * (data['result'][0]['Last']) - (data['result'][0]['PrevDay']) / data['result'][0]['PrevDay']
+            ret = '```{} {}฿ ({:.2f}%) ${:.5f} (Bittrex)```'.format(
+                str_usr.upper(),
+                data['result'][0]['Last'],
+                perc,
+                data['result'][0]['Last'] * val_btc
+            )
+        else:
+            ret = data['result'][0]['Last'] * val_btc
+    else:
+        ret = 0
+    return ret
+
+def btcrecup():
+    """ Get BTC price"""
+    url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,EUR'
+    content = requests.get(url)
+    data = content.json()
+    return data['EUR'], data['USD']
 
 @client.event
 async def on_message(message):
@@ -323,33 +295,36 @@ async def on_message(message):
                     'Es-tu sûr de respecter les règles d\'utilisation du bot ? ;)'
                 )
 
-    if message.content.startswith(('price','Price','prix','Prix')):
+    if message.content.startswith(('price', 'Price', 'prix', 'Prix')):
         await client.send_typing(message.channel)
-        retour=traitement(message.content)
+        retour = traitement(message.content)
         for ret in retour:
             if ret:
                 await client.send_message(
                     message.channel,
-                    retour[i]
+                    ret
                 )
 
-    if message.content.startswith(('conv','Conv')):
+    if message.content.startswith(('conv', 'Conv')):
         await client.send_typing(message.channel)
-        retour=traitement(message.content)
+        retour = traitement(message.content)
         if retour:
             await client.send_message(
                 message.channel,
                 retour
             )
 
-    if message.content.startswith(('calc','Calc')):
-        retour=calc(message.content)
+    if message.content.startswith(('calc', 'Calc')):
+        retour = calc(message.content)
         await client.send_message(
             message.channel,
-            "```"+ ((message.content).split())[1]+ " = " + str("%.2f" %retour)+"```"
+            '```{} = {}```'.format(
+                message.content.split()[1],
+                retour
+            )
         )
 
-    if message.content.startswith(('chart','Chart')):
+    if message.content.startswith(('chart', 'Chart')):
         await client.send_typing(message.channel)
         retour = traitement(message.content)
         if retour:
